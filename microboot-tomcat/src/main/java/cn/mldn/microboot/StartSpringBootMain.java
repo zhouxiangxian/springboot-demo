0769d2a93156facd41566faf0fa7d28d9762dde6
dell <dell@dell-PC>
2019-02-15 17:45:14 +0800

    add microboot-tomcat

--- microboot-tomcat/src/main/java/cn/mldn/microboot/StartSpringBootMain.java
+++ microboot-tomcat/src/main/java/cn/mldn/microboot/StartSpringBootMain.java
@@ -2,10 +2,19 @@ package cn.mldn.microboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication	// 启动SpringBoot程序，而后自带子包扫描
-public class StartSpringBootMain {
+@ComponentScan("cn.mldn.microboot")
+public class StartSpringBootMain  extends SpringBootServletInitializer{
     public static void main(String[] args) throws Exception {
         SpringApplication.run(StartSpringBootMain.class, args);
+    }
+    
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    	return builder.sources(StartSpringBootMain.class);
     }
 }
